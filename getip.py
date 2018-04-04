597fd553c618b94a469986d57ef9fbf86183d307
slevin <lundy11111@gmail.com>
2018-04-04 08:53:31 +0100

    update

--- getip.py
+++ getip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #
 # Gets your public IP address
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='getip2',
-    version='1.2',
+    version='1.3',
     description='Gets your public IP Address',
     author='JG',
     author_email='lundy11111@gmail.com',
