1ece0a656e8f9657bbbdcc89e04ab328cfddaf07
slevin <lundy11111@gmail.com>
2018-04-05 04:13:38 +0100

    update project structure

---
+++ src/setup/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='getip2',
+    version='1.6',
+    description='Gets your public IP Address',
+    author='JG',
+    author_email='so5handicapper@live.co.uk',
+    url='https://github.com/4A47/getip2',
+    py_modules=['getip'])
